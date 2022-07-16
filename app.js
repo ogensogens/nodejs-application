fe8d291c5f29e2f80c10ae56b9a5574c80c2c1c2
ogensogens <105896389+ogensogens@users.noreply.github.com>
2022-07-15 23:14:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cfenv = require('cfenv');
 var app  =  express(); 
 var appEnv = cfenv.getAppEnv();
 var url = process.env.url
-app.set('port', (process.env.PORT || 3000))
+app.set('port', (process.env.PORT || 9981))
 app.use(express.static(__dirname + '/images'))
 
 
@@ -75,7 +75,7 @@ app.get('/redirect', function(req, res) {
     
  
     app.listen(app.get('port'), function() {
-        console.log("Node JS app is running at http://localhost" + app.get('3000') +"/acadalearning");
+        console.log("Node JS app is running at http://localhost" + app.get('9981') +"/acadalearning");
       })
     
 
